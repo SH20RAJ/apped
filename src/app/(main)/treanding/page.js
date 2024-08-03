f485c1ed754cad6cdb784932d42be7c796ec1ae9
sh20raj <shaswatraj3@gmail.com>
2024-08-04 01:31:18 +0530

    refactor: Update trending page to display grossing and top paid apps

--- src/app/(main)/treanding/page.js
+++ src/app/(main)/treanding/page.js
@@ -4,12 +4,17 @@ import gplay from 'google-play-scraper'
 export default async function page() {
   let apps = await gplay.list({
     num: 200,
-    category: gplay.category.SOCIAL
+    collection: gplay.collection.GROSSING,
+  });
+  let apps2 = await gplay.list({
+    num: 200,
+    collection: gplay.collection.TOP_PAID,
   });
 
   return (
     <div>
-      <PopularApps  apps={apps} title='Games' />
+      <PopularApps  apps={apps} title='Grossing' />
+      <PopularApps  apps={apps2} title='Top Paid' />
     </div>
   )
 }
