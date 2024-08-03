2a9275afe270757269d1c926f9bd261a0e8550fc
sh20raj <shaswatraj3@gmail.com>
2024-08-04 01:14:08 +0530

    logo.png

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        
+        <link rel="icon" href="/logo.png" />
       
       </head>
       <body className={inter.className}>{children}</body>
