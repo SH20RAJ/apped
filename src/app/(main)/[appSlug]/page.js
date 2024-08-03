19f86bddd5fd5771dfc574b01299b2e1db0c3da3
sh20raj <shaswatraj3@gmail.com>
2024-08-04 01:28:34 +0530

    apps developers page okkkk

---
+++ src/app/(main)/[appSlug]/page.js
@@ -0,0 +1,17 @@
+import gplay from "google-play-scraper";
+import PopularApps from "../_components/PopularApps";
+
+export default async function page({ params }) {
+  const { appSlug } = params;
+  console.log(appSlug);
+
+  let apps = await gplay
+    .developer({ devId: appSlug || "DxCo Games" })
+    .then(console.log);
+
+  return (
+    <div>
+      <PopularApps apps={apps} />
+    </div>
+  );
+}
