11ecddd9450ccb27ef57a92a7fcffc328c0695d2
sh20raj <shaswatraj3@gmail.com>
2024-08-05 13:18:25 +0530

    Sitemaps

---
+++ src/app/(sitemap)/apps/sitemap.js
@@ -0,0 +1,40 @@
+
+import gplay from 'google-play-scraper'
+
+
+export default async function sitemap() {
+
+  let sitemap = []
+
+  let apps = await gplay.list({
+    category: gplay.category.MUSIC_AND_AUDIO,
+  })
+
+  apps.concat(await gplay.list({
+    category: gplay.category.GAME_SPORTS,
+    collection: gplay.collection.TOP_FREE,
+  }))
+  apps.concat(await gplay.list({
+    collection: gplay.collection.TOP_PAID,
+  }))
+  apps.concat(await gplay.list({
+    collection: gplay.collection.TOP_GROSSING,
+  }))
+  apps.concat(await gplay.list({
+    collection: gplay.collection.TOP_FREE,
+    num: 500
+  }))
+
+  apps.forEach(app => {
+    sitemap.push({
+      url: `https://apped.me/app/${app.appId}`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.9,
+    })
+  }
+  )
+
+
+    return sitemap
+  }
---
+++ src/app/(sitemap)/games/sitemap.js
@@ -0,0 +1,45 @@
+import gplay from "google-play-scraper";
+
+export default async function sitemap() {
+  let sitemap = [];
+
+  let apps = await gplay.list({
+    category: gplay.category.APPLICATION,
+    num: 250,
+  });
+
+  apps.concat(
+    await gplay.list({
+      category: gplay.category.LIBRARIES_AND_DEMO,
+      collection: gplay.collection.TOP_FREE,
+    })
+  );
+  apps.concat(
+    await gplay.list({
+      collection: gplay.collection.TOP_PAID,
+    })
+  );
+  apps.concat(
+    await gplay.list({
+      collection: gplay.collection.TOP_FREE,
+      category: gplay.category.EDUCATION,
+    })
+  );
+  apps.concat(
+    await gplay.list({
+      collection: gplay.collection.TOP_FREE,
+      num: 500,
+    })
+  );
+
+  apps.forEach((app) => {
+    sitemap.push({
+      url: `https://apped.me/app/${app.appId}`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.9,
+    });
+  });
+
+  return sitemap;
+}
