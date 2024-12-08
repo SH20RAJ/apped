488045ef146a0a207c01a7bf5a582c6281b6d9d1
sh20raj <shaswatraj3@gmail.com>
2024-12-09 02:57:05 +0530

    Add Google Ads script and create ads.txt file

--- src/app/layout.js
+++ src/app/layout.js
@@ -67,6 +67,8 @@ export default function RootLayout({ children }) {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1828915420581549"
           crossOrigin="anonymous"
         ></script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1828915420581549"
+     crossorigin="anonymous"></script>
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-8XJ794KHGF"
